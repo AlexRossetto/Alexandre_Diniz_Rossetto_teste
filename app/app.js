41834ad2ab1bbf75d04d9eb54ddfd4324619a5cb
Alexandre Diniz Rossetto <alexandredrossetto@hotmail.com>
2018-01-25 03:29:27 -0200

    few modifications

--- app/app.js
+++ app/app.js
@@ -5,26 +5,26 @@ var myApp = angular.module('myApp', [
 
 myApp.config(function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/view1');
+    $urlRouterProvider.otherwise('/Cadastro-Passageiros');
 
     $stateProvider
 
         // HOME STATES AND NESTED VIEWS ========================================
         .state('view1', {
-            url: '/view1',
+            url: '/Cadastro-Passageiros',
             templateUrl: 'view1/view1.html',
             controller: 'View1Ctrl'
         })
 
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('view2', {
-            url: '/view2',
+            url: '/Cadastro-Motoristas',
             templateUrl: 'view2/view2.html',
             controller: 'View2Ctrl'       
         })
 
         .state('view3', {
-            url:'/view3',
+            url:'/Cadastro-Corridas',
             templateUrl: 'view3/view3.html',
             controller: 'View3Ctrl'
         })
